4769b210b5fd8a79ffd7db36c16ed0e97a0a746c
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2022-10-06 00:36:31 -0500

    Added search-input

---
+++ components/SearchInput.js
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { Dimensions, StyleSheet, TextInput, View } from 'react-native';
+import { ActionButton } from './ActionButton';
+
+const { width, height } = Dimensions.get('window');
+
+export const SearchInput = () => {
+  const [search, setSearch] = useState('');
+
+
+  //TODO: Add logic to search from API
+  const handleSearch = () => {
+    console.log(search);
+    if (search?.length >= 3) {
+      if (isNaN(search)) {
+        console.log(`Searching for name: ${search}`);
+      } else {
+        console.log(`Searching for exp: ${search}`);
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Buscar por Nombre o Expediente"
+        value={search}
+        onChange={value => setSearch(value)}
+      />
+      <ActionButton
+        icon="search"
+        color="#000"
+        backgroundColor="#FFF"
+        text="Buscar"
+        handler={handleSearch}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '4%'
+  },
+  input: {
+    width: width * 0.6,
+    height: 32,
+    margin: height*0.01,
+    borderBottomWidth: 1,
+		borderBottomColor:'black',
+    color:'#000000',
+    fontFamily:'Fredoka-Light',
+    backgroundColor:'#ffffff',
+    fontSize:width * .04,
+  },
+});
