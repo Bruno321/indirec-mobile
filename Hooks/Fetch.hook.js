64d401499abe2bd817e7d6b2c1621b875ea89d6e
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2022-11-08 10:40:43 -0600

    Added custom hook to handle fetch requests

---
+++ Hooks/Fetch.hook.js
@@ -0,0 +1,35 @@
+import { Alert } from 'react-native';
+import { useState, useEffect, useCallback } from 'react';
+import { FIND, process } from '../Service/Api';
+
+export const useFetchData = (path) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getData = useCallback(async () => {
+    setLoading(true);
+    const response = await process(FIND, path, {}, {});
+
+    if (response?.data.ok) {
+      setData(response.data.data);
+    } else {
+      Alert.alert(
+        'Error', 
+        `No se pudo consultar hacia ${path}`,
+        [
+          {text:'Okay'},
+        ]
+      );
+    }
+
+    setLoading(false);
+  }, [process]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  const update = () => getData();
+
+  return [data, loading, update];
+};
