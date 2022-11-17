e6660687adcf69c5c3ce4aa104c4b2d11187ac4f
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2022-11-17 11:47:49 -0600

    Fix for TouchableCmp

--- assetsUI/TouchableCmp.js
+++ assetsUI/TouchableCmp.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Platform} from 'react-native';
-import {TouchableOpacity, TouchableNativeFeedback} from "react-native-gesture-handler";
+import { Platform } from 'react-native';
+import { TouchableOpacity, TouchableNativeFeedback } from "react-native";
 
 const TouchableCmp = (props) =>{
-    const TouchableComponent = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
-    return(
-        <TouchableComponent onPress={props.onPress} style={props.style} background={props.background}>
-            {props.children}    
-        </TouchableComponent>
-    )
+  const TouchableComponent = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+  return(
+      <TouchableComponent onPress={props.onPress} style={props.style} background={props.background}>
+          {props.children}    
+      </TouchableComponent>
+  )
 }
 
 export default TouchableCmp;
