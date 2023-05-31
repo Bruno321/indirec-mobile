e8e8c06e4dbcf5d92acf8b7772ac687277eca770
Sebastian Acosta <eabastian@gmail.com>
2023-05-31 14:12:37 -0600

    Added equipos pdf download functionality

--- Screens/Equipos.details.js
+++ Screens/Equipos.details.js
@@ -1,18 +1,86 @@
-import { Dimensions, StyleSheet, View, SafeAreaView, Text, ScrollView, Image} from 'react-native';
-import { Header, Col, Row } from '../components';
+import { Dimensions, StyleSheet, View, SafeAreaView, Text, ScrollView, Image, Alert } from 'react-native';
+import { Header, Col, Row, ActionButton } from '../components';
+import * as FileSystem from 'expo-file-system';
+import * as MediaLibrary from 'expo-media-library';
+import * as Sharing from 'expo-sharing';
+import { URL, process, SAVE } from '../Service/Api';
+
+const GRANTED = "granted";
 
 const { width, height, fontScale } = Dimensions.get('window');
 
-const LargeText = ({ children, style = {}, numberOfLineas}) => {
+const LargeText = ({ children, style = {}, numberOfLineas }) => {
 
     return (
-      <Text style={{ fontSize: width * 0.038 / fontScale, ...style }} numberOfLines={numberOfLineas}>{children}</Text>
+        <Text style={{ fontSize: width * 0.038 / fontScale, ...style }} numberOfLines={numberOfLineas}>{children}</Text>
     );
-  };
+};
 
-export const EquiposDetails = ({ navigation, route}) => {
+export const EquiposDetails = ({ navigation, route }) => {
+    const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
     const { data } = route.params;
+    console.log("dataid " + data.id)
     console.log(data);
+
+    const handleDownload = async () => {
+        console.log("id: " + data.id)
+        const response = await process(SAVE, 'equipo-pdf', {
+            id: data.id,
+        }).catch(e=>{
+            console.log("catch " + e)
+        })
+        console.log("response "+ JSON.stringify(response.data.pdf))
+        if (response.status === 201) {
+            var link = `${URL}/pdf/${response.data.pdf}`;
+            const { status } = await requestPermission();
+            if (status === GRANTED) {
+                saveFileToGallery(link, status);
+            } else {
+                console.log(permissionResponse)
+                let permissionStatus;
+                permissionStatus = await requestPermission();
+            }
+        } else {
+          console.log(response);
+        }
+    };
+
+    const saveFileToGallery = async (fileUrl, status) => {
+        console.log("url " + fileUrl)
+        try {
+            if (status !== GRANTED && permissionResponse.status !== GRANTED) {
+                console.log('Permiso denegado para acceder a la galería.');
+                return;
+            }
+
+            const fileExtension = fileUrl.split('.').pop().toLowerCase();
+            console.log("file extension " + fileExtension)
+            const fileUri = FileSystem.cacheDirectory + `file.${fileExtension}`;
+            console.log("file uri " + fileUri)
+            await FileSystem.downloadAsync(fileUrl, fileUri);
+
+            if (fileExtension === 'pdf') {
+                await Sharing.shareAsync(fileUri);
+                console.log('PDF guardado en la galería.');
+                Alert.alert('Descarga completa', 'Documento guardado en la galería', [
+                    { text: 'OK', onPress: () => console.log('OK Pressed') },
+                ]);
+            } else {
+                await MediaLibrary.saveToLibraryAsync(fileUri);
+                console.log('Imagen guardada en la galería.');
+                Alert.alert('Descarga completa', 'Documento guardado en la galería', [
+                    { text: 'OK', onPress: () => console.log('OK Pressed') },
+                ]);
+            }
+        } catch (error) {
+            console.log('Error al guardar el archivo:', error);
+            Alert.alert('Error en descarga', 'Intentelo más tarde', [
+                { text: 'OK', onPress: () => console.log('OK Pressed') },
+            ]);
+        }
+    }
+
+
     return (
         <View style={styles.main}>
             <SafeAreaView style={{ backgroundColor: "#003070" }} />
@@ -20,7 +88,7 @@ export const EquiposDetails = ({ navigation, route}) => {
             <ScrollView contentContainerStyle={styles.centeredView} bounces={false} overScrollMode="never">
                 <View style={styles.equipoPresentation}>
                     <Text style={styles.equipoName}>{data.nombre}</Text>
-                    <Image source={require('../images/ImagenEjemploEquipo.png')} style={styles.equipoImage}/>
+                    <Image source={require('../images/ImagenEjemploEquipo.png')} style={styles.equipoImage} />
                     <Text style={styles.equipoFacultad}>{data.facultad}</Text>
                     <Text style={styles.equipoCampus}>{data.campus}</Text>
                 </View>
@@ -42,6 +110,14 @@ export const EquiposDetails = ({ navigation, route}) => {
                         <LargeText style={styles.dato}>{data.nombreAsistente + " " + data.apellidoAsistente}</LargeText>
                     </Row>
                 </View>
+                <ActionButton
+                    text="Descargar PDF"
+                    handler={() => handleDownload()}
+                    backgroundColor="#FFF"
+                    color="#003070"
+                    icon="file-pdf-o"
+                    style={{ width: "80%", marginTop: '10%', alignSelf: 'center', height: 60, marginBottom: 5, borderWidth: 2, borderColor: "#003070", borderRadius: 18 }}
+                />
             </ScrollView>
         </View>
     );
@@ -56,27 +132,27 @@ const styles = StyleSheet.create({
     centeredView: {
         backgroundColor: '#FFF',
     },
-    equipoPresentation:{
+    equipoPresentation: {
         width: width,
         paddingVertical: 20,
         alignItems: 'center',
     },
-    equipoName:{
-        fontSize: 30/fontScale,
+    equipoName: {
+        fontSize: 30 / fontScale,
         fontWeight: '400',
     },
-    equipoImage:{
+    equipoImage: {
         width: 180,
         height: 180,
         borderRadius: 18,
         overflow: 'hidden',
     },
-    equipoFacultad:{
-        fontSize: 26/fontScale,
+    equipoFacultad: {
+        fontSize: 26 / fontScale,
         fontWeight: '400',
     },
-    equipoCampus:{
-        fontSize: 23/fontScale,
+    equipoCampus: {
+        fontSize: 23 / fontScale,
         fontWeight: '300',
     },
     boldText: {
@@ -86,11 +162,11 @@ const styles = StyleSheet.create({
         width: "40%",
         paddingHorizontal: "5%",
         marginTop: 10,
-      },
-      dato: {
+    },
+    dato: {
         fontSize: 18 / fontScale,
         width: "55%",
         marginTop: 10,
-      },
-    
+    },
+
 });
