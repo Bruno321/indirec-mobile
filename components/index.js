85737e02467718889f4ea302a800251ed5a93769
Jorge Bernal <jorgealejandrobernal@gmail.com>
2023-06-02 09:39:42 -0600

    Removed unused components

--- components/index.js
+++ components/index.js
@@ -5,7 +5,6 @@ export * from './DeportistasCard';
 export * from './EquiposCard';
 export * from './EventosCard';
 export * from './Filters';
-export * from './OldFilters';
 export * from './Grid.components';
 export * from './Header';
 export * from './List';
